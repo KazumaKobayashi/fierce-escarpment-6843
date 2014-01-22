2f1846d96c01d28c45a2d81472b8f8bb4dee7cf2
Kazuki Hasegawa <hasegawa_0204@hotmail.co.jp>
2014-01-22 15:51:47 +0900

    Add EscapeUtil

---
+++ src/main/java/com/example/util/EscapeUtil.java
@@ -0,0 +1,15 @@
+package com.example.util;
+
+/**
+ * エスケープ処理のUtilクラス
+ *
+ * @author Kazuki Hasegawa
+ */
+public final class EscapeUtil {
+	private EscapeUtil() {}
+
+	public static String escape(String str) {
+		// TODO: エスケープ処理
+		return str;
+	}
+}
