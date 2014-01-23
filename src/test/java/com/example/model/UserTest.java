06164969a5c07facfdcabc74f3ce92fb8c126cfc
Kazuki Hasegawa <hasegawa_0204@hotmail.co.jp>
2014-01-23 23:55:19 +0900

    Small changes.

--- src/test/java/com/example/model/UserTest.java
+++ src/test/java/com/example/model/UserTest.java
@@ -9,21 +9,28 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import com.example.AbstractTest;
+import com.jayway.jsonassert.JsonAssert;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 /**
+ * ユーザモデルのテスト
+ *
  * @author Kazuki Hasegawa
  */
 @RunWith(JUnit4.class)
-public class UserTest extends AbstractTest {
+public class UserTest extends AbstractModelTest {
+	private String id = UserTest.class.getName();
+
+	/**
+	 * UserモデルのJSON文字列変換テスト
+	 *
+	 * @throws IOException
+	 */
 	@Test
 	public void JSON文字列変換テスト() throws IOException {
-		String id = "Kazuma";
+		// 必要なデータを作成
 		Double lat = 45.1235, lng = 41.345;
-
 		Coordinate coord = new Coordinate();
 		coord.setLng(lng);
 		coord.setLat(lat);
@@ -33,15 +40,11 @@ public class UserTest extends AbstractTest {
 		user.setCoord(coord);
 
 		String json = mapper.writeValueAsString(user);
-		StringBuilder builder = new StringBuilder("{\"username\":\"");
-		builder.append(id);
-		builder.append("\",\"lat\":");
-		builder.append(lat);
-		builder.append(",\"lng\":");
-		builder.append(lng);
-		builder.append(",\"id\":\"");
-		builder.append(id);
-		builder.append("\"}");
-		assertThat(json, is(builder.toString()));
+		JsonAssert
+			.with(json)
+			.assertThat("$.id", is(id))
+			.assertThat("$.username", is(id))
+			.assertThat("$.lat", is(lat))
+			.assertThat("$.lng", is(lng));
 	}
 }
