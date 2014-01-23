9298047dc1defd13244f0031e75fd2e8229eaa21
Kazuki Hasegawa <hasegawa_0204@hotmail.co.jp>
2014-01-23 14:21:23 +0900

    Add controller tests.

---
+++ src/test/java/com/example/controller/RegisterControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+/**
+ * RegisterControllerのテスト
+ *
+ * @author Kazuki Hasegawa
+ * @see com.example.controller.RegisterController
+ */
+@Transactional
+@TransactionConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = "classpath:testContext.xml")
+public class RegisterControllerTest extends AbstractControllerTest {
+	private String id = RegisterControllerTest.class.getName();
+	private String password = "kazumakobayashi";
+
+	@Test
+	public void  登録テスト() throws Exception {
+		mockMvc.perform(post("/register")
+						.param("id", id)
+						.param("password", password))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType("application/json"))
+			// TODO: Successコードと比較
+			.andExpect(jsonPath("$.code").value(0));
+	}
+}
